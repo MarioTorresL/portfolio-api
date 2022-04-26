5a568adadc685ba659f310e60dd8a1304335ef73
Mario Torres <mario.torreslepe@gmail.com>
2022-04-26 18:27:00 -0400

    finish first configurations and run server test

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const app = express();
-const port = 3000
 
-app.get('/', (req,res) =>{
-  res.send('hello world')
+const app = require ('./src/index.js');
+
+const {port} = require('./config.json');
+
+app.listen(port, () =>{
+  console.log(`App listening at http://localhost:${port}`)
 })
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express()
+const server = require('http').Server(app);
+
+module.exports= server;
