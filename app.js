6239b199c09e58b2e209e246ae0abd72c618523a
Mario Torres <mario.torreslepe@gmail.com>
2022-05-10 17:45:26 -0400

    minor changes variable names

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = require ('./src/index.js');
 
 const {PORT} = require('./config.json');
 
-app.listen(port, () =>{
+app.listen(PORT, () =>{
   console.log(`App listening at http://localhost:${PORT}`)
 })
