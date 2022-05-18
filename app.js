f6ce0ae69accb09c1df3ee9ff90a1ff654e3346c
Mario Torres <mario.torreslepe@gmail.com>
2022-05-18 04:09:22 -0400

    fixes port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require ('./src/index.js');
 
 const {PORT} = require('./config.json');
 
-const port = process.env.PORT || PORT
+const port = process.env.DB_PORT || PORT
 
 app.listen(port, () =>{
   console.log(`App listening at http://localhost:${port}`)
