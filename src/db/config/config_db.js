2a3cf7e044a3f654ddce04a3200596b7426d1089
Mario Torres <mario.torreslepe@gmail.com>
2022-05-18 02:51:06 -0400

    add config db production

--- src/db/config/config_db.js
+++ src/db/config/config_db.js
@@ -16,10 +16,10 @@ module.exports={
     "dialect": config.DB_DIALECT
   },
   "production": {
-    "username": config.DB_USER,
-    "password": config.DB_PASS,
-    "database": config.DB_NAME,
-    "host": config.DB_HOST,
-    "dialect": config.DB_DIALECT
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   }
 }
