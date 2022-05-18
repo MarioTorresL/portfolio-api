6b9d0922a29191de85b50e61e3fc280012f72cd3
Mario Torres <mario.torreslepe@gmail.com>
2022-05-18 03:32:57 -0400

    add db_url env

--- src/db/config/config_db.js
+++ src/db/config/config_db.js
@@ -20,6 +20,7 @@ module.exports={
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "databaseurl": process.env.DATABASE_URL
   }
 }
