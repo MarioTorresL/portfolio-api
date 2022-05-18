f497733ded378516b1f64a1660594dc8410c4dbf
Mario Torres <mario.torreslepe@gmail.com>
2022-05-18 02:34:17 -0400

    changes to deploy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = require ('./src/index.js');
 
 const {PORT} = require('./config.json');
 
-app.listen(PORT, () =>{
-  console.log(`App listening at http://localhost:${PORT}`)
+const port = process.env.PORT || PORT
+
+app.listen(port, () =>{
+  console.log(`App listening at http://localhost:${port}`)
 })
