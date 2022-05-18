ea3617666c77ec431b2fe7db265427edfbe50a81
Mario Torres <mario.torreslepe@gmail.com>
2022-05-18 02:02:28 -0400

    minor changes errors deploy

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = require ('./src/index.js');
 
 const {PORT} = require('./config.json');
 
-app.listen(PORT, process.env.PORT, () =>{
-  console.log(`App listening at http://localhost:${PORT}`, process.env.PORT)
+app.listen(PORT, () =>{
+  console.log(`App listening at http://localhost:${PORT}`)
 })
