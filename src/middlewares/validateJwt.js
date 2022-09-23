776a588539c7195b1b5e82897332123ad1a5e8b2
Mario Torres <mario.torreslepe@gmail.com>
2022-09-23 15:35:54 -0300

    fix validate token midleware, add parameters validator in users routes

--- src/middlewares/validateJwt.js
+++ src/middlewares/validateJwt.js
@@ -2,26 +2,40 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next)=>{
   if( !req.headers['authorization'] ){
-    return res.status(401).send('Authorization header not present');
+    return res.status(401).json({
+      message:'Authorization header not present',
+      error: 'Authorization'
+    });
   }
 
   const authorizationHeader = req.headers['authorization'];
   const [type, accessToken] = authorizationHeader.split(' ');
 
   if( type !== 'Bearer' ){
-    return res.status(401).send('Wrong Authorization header type given');
+    return res.status(401).json({
+      message:'Wrong Authorization header type given',
+      error: 'Authorization'
+    });
   }
 
   if( !accessToken ){
-    return res.status(401).send('Access token not present')
+    return res.status(401).json({
+      message:'Access token not present',
+      error: 'Authorization'
+    })
   }
 
   try{
     const decoded = jwt.verify(accessToken, process.env.SECRET_KEY);
     req.userId = decoded.id;
+
   }catch(e){
-    return res.status(401).send('InvalidToken')
+    return res.status(401).json({
+      message: 'Invalid Token',
+      error: 'Authorization'
+    })
   }
+
   return next();
 }
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
+const { check } = require('express-validator');
+
+const { validateParams } = require('../middlewares/validateParams');
+const { verifyToken } = require('../middlewares/validateJwt')
 
 const {getUsers, postUser, putUser, deleteUser } = require('../controllers/users');
 //=== /api/comments ===
 
-router.get('/', getUsers);
-router.post('/', postUser);
+router.get('/', verifyToken, getUsers);
+
+router.post('/', 
+  [
+    check('firstName', 'first name is required').not().isEmpty(),
+    check('lastName', 'last name is required').not().isEmpty(),
+    check('userName', 'username is required').not().isEmpty(),
+    check('encryptedPassword', 'password is required minum 6 characters').not().isEmpty().isLength({min: 6}),
+    check('email', 'email is required').not().isEmpty().isEmail().normalizeEmail(),
+  ],
+  validateParams,
+  postUser);
+
 router.put('/:id', putUser);
+
 router.delete('/:id', deleteUser);
 
 module.exports = router;
