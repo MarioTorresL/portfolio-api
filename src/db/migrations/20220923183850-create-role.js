b9bcd1e98a4b04a53d3fb85db9b05dea5fdb4771
Mario Torres <mario.torreslepe@gmail.com>
2022-09-23 15:57:48 -0300

    add role table

---
+++ src/db/migrations/20220923183850-create-role.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable('Roles',{
+      id:{
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name:{
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique:true
+      },
+      createdAt:{
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      updatedAt:{
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      deletedAt:{
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable('Roles')
+  }
+};
---
+++ src/db/migrations/20220923185227-add-column-RoleId-users-table.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('Users', 'RoleId', {
+      type: sequelize.DataTypes.INTEGER,
+      allowNull: false
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn('Users', 'RoleId')
+  }
+};
