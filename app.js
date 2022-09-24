07d780d285aeff8026eaa9c07ff8b74eac856ff7
Mario Torres <mario.torreslepe@gmail.com>
2022-09-24 13:26:02 -0300

    fix listen port in app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const app = require('./src/index.js');
 require ('dotenv').config()
 
-if(!process.env.NODE_ENV){
-  app.listen(process.env.PORT, () =>{
-    console.log(`App listening at http://localhost:${process.env.PORT}`)
-  })
-}else{
-  app.get('/', (req, res) => res.status(200).json("El servidor Funciona!"));
-}
+const port = process.env.PORT || 3000;
+
+app.listen(port , () =>{
+  console.log(`App listening at http://localhost:${process.env.PORT}`)
+})
