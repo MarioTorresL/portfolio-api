4b6a30d19f85607bc74b641c99b548893c0e37a0
Mario Torres <mario.torreslepe@gmail.com>
2022-11-09 20:35:20 -0300

    add log models index

--- database/models/index.js
+++ database/models/index.js
@@ -9,6 +9,7 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
+console.log('config', config)
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
